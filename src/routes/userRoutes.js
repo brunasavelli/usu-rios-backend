1f9c92695fdebdfa53c8b2eecdae3b2759ec2e99
Bruna Savelli <bnsavelli@gmail.com>
2025-02-20 19:28:53 -0300

    feat: create routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+
+router.get('/users', usersController.getAllUsers);
+router.post('/users', usersController.addUser);
+router.get('/users/:id', usersController.getUserById);
+router.put('/users/:id', usersController.updateUser);
+router.delete('/users/:id', usersController.deleteUser);
+
+module.exports = router;
